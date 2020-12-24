42afd407d3211cef02ac32726cbd6b27daafda6b
Laura Nielsen <lnielsen.wi@gmail.com>
2020-12-24 00:42:46 -0600

    RecursionPrompt 10 Palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -171,6 +171,15 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+  string.replace(/\s+/g, '');
+  if (string.length === 0 || string.length === 1) {
+    return true;
+  }
+  if (string[0].toLowerCase() === string[string.length-1].toLowerCase()) {
+    return palindrome(string.slice(1,string.length-1));
+  } else {
+    return false;
+  }
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
